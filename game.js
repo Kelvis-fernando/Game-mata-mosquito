d4d3fac66bf41ee25ffe29dac73eac786f415b35
Kelvis Fernando <kelvisdelapena@gmail.com>
2021-03-17 11:09:28 -0300

    Adicionado o cronometro para o game, adicionado tambem a tela de vitoria e a tela inicial, e tambem adicionado a logica de seleção de dificuldade

--- game.js
+++ game.js
@@ -1,6 +1,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 15
 
 function ajusteTamanhoPalcoJogo() {
     altura = window.innerHeight
@@ -10,6 +11,19 @@ function ajusteTamanhoPalcoJogo() {
 }
 
 ajusteTamanhoPalcoJogo()
+
+var cronometro = setInterval( function () {
+    tempo -= 1
+
+    if (tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(limpaMosquito)
+        window.location.href = 'você-ganhou.html'
+    }else {
+        document.getElementById('cronometro').innerHTML = tempo 
+    }
+
+}, 1000)
 
 function posicaoRandomica() {
 
